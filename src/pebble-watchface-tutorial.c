89bfc07b14a59faf5d69b4d9378824d2ed3ca460
Gopinath Danda <gopinath.danda@gmail.com>
2014-11-01 04:36:10 -0400

    Day functionality added.

--- src/pebble-watchface-tutorial.c
+++ src/pebble-watchface-tutorial.c
@@ -2,8 +2,10 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
+static TextLayer *s_day_layer;
 static GFont s_time_font;
 static GFont s_date_font;
+static GFont s_day_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -15,6 +17,7 @@ static void update_time() {
   // Create a long-lived buffer
   static char buffer[] = "00:00";
   static char date_buffer[] = "00.00";
+  static char day_buffer[] = "Thu";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
@@ -28,10 +31,14 @@ static void update_time() {
   // Write the current date into the date buffer
   strftime(date_buffer, sizeof("00.00"), "%m.%d", tick_time);
 
+  // Write the current day into the day buffer
+  strftime(day_buffer, sizeof("Thu"), "%a", tick_time);
+
 
   // Display this time and date on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
   text_layer_set_text(s_date_layer, date_buffer);
+  text_layer_set_text(s_day_layer, day_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -42,6 +49,7 @@ static void main_window_load(Window *window) {
   // Create new font
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PLAY_FONT_REGULAR_35));
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PLAY_FONT_BOLD_20));
+  s_day_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PLAY_FONT_REGULAR_20));
 
   // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_MAIN_WATCHFACE);
@@ -65,13 +73,23 @@ static void main_window_load(Window *window) {
   s_date_layer = text_layer_create(GRect(0, 142, 144, 26));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_text(s_date_layer,"10.31");
+  //text_layer_set_text(s_date_layer,"10.31");
   // Apply to TextLayer
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-
-  
+  // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+
+  // Create day TextLayer
+  s_day_layer = text_layer_create(GRect(0, 0, 144, 26));
+  text_layer_set_background_color(s_day_layer, GColorClear);
+  text_layer_set_text_color(s_day_layer, GColorWhite);
+  //text_layer_set_text(s_date_layer,"Mon");
+  // Apply to TextLayer
+  text_layer_set_font(s_day_layer, s_day_font);
+  text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_layer));
 
   // Make sure the time is displayed from the start
   update_time();
@@ -81,6 +99,7 @@ static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
 	text_layer_destroy(s_date_layer);
+	text_layer_destroy(s_day_layer);
   
     // Unload GFont
     fonts_unload_custom_font(s_time_font);
